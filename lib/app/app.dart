ee5b33fc0849abbb52033849740b758582ae312e
Allan Artuso <allan.artuso@gmail.com>
2024-07-06 14:39:29 +0200

    feat: add i18n support

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:form_builder_validators/localization/l10n.dart';
 import 'router.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
   final ThemeData theme;
@@ -9,10 +12,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-        routeInformationProvider: AppRouter.router.routeInformationProvider,
-        routeInformationParser: AppRouter.router.routeInformationParser,
-        routerDelegate: AppRouter.router.routerDelegate,
-        debugShowCheckedModeBanner: false,
-        theme: theme);
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routerDelegate: AppRouter.router.routerDelegate,
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        FormBuilderLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
+      locale: const Locale('en'),
+    );
   }
 }
--- lib/libs/features/home/home.dart
+++ lib/libs/features/home/home.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Home',
-        style: TextStyle(color: Colors.white),
-      ),
+    return Center(
+      child: Text(AppLocalizations.of(context)!.helloWorld),
     );
   }
 }
